994fc5aeb9b20fa5b05bc6a434523fa75dc606c8
MicuAlexandruVlad <micualexandru131@yahoo.com>
2019-08-05 01:10:11 +0300

    Created the ui for login and sign up pages

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  email: string;
+  password: string;
+
+  navigateSignUp() {
+    this.router.navigate(['../sign-up'], { relativeTo: this.route });
+  }
+
+  authenticateUser() {
+    console.log(this.email);
   }
 
 }
